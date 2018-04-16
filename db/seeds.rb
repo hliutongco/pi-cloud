7b73f116a20c6dd30d86a95c6b9ef95ef109a071
Helen Liutongco <liutongcoh@gmail.com>
2018-04-16 11:33:31 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "lilruby", email: "lilrubyrox@gmail.com", password: "lilrubyrox", bio: "lil ruby rox", img: "https://cdn-images-1.medium.com/max/1600/1*p0uOGL9JugR5rAQAu8pujA.png")
+User.create(name: "lilbooboo", email: "lilbooboo@gmail.com", password: "lilbooboorox", bio: "lil booboo rox", img: "http://berryripe.com/wp-content/uploads/2012/10/band-aid.jpg")
+User.create(name: "lildiamond", email: "lildiamond@gmail.com", password: "lildiamondrox", bio: "lil diamond rox", img: "https://assets.ritani.com/assets/education_channel/diamonds/diamond-anatomy-27680883da28fb94afdbd63f43b03f48.png")
+
+Song.create(song_url: "http://testurl.com", code: "this is a test", title: "awesome song", genre: "conscious rap", img: "https://cdn-images-1.medium.com/max/1600/1*p0uOGL9JugR5rAQAu8pujA.png")
+Song.create(song_url: "http://testurl2.com", code: "this is a second test", title: "sick beats", genre: "trap", img: "http://berryripe.com/wp-content/uploads/2012/10/band-aid.jpg")
+Song.create(song_url: "http://testurl3.com", code: "this is a third test", title: "slow jam", genre: "smooth jazz", img: "https://assets.ritani.com/assets/education_channel/diamonds/diamond-anatomy-27680883da28fb94afdbd63f43b03f48.png")
+
+Playlist.create(name: "Hot Playlist", description: "this s*** is tight")
