fa1f0eb2039e18d0010bbd1608a9694371e6a249
Al <lin.alice72@gmail.com>
2018-04-16 11:52:59 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416145344) do
+ActiveRecord::Schema.define(version: 20180416154602) do
 
-  create_table "songs", force: :cascade do |t|
-    t.string "file_url"
-    t.string "code"
-    t.string "title"
-    t.string "genre"
-    t.string "img"
+  create_table "playlists", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_songs", force: :cascade do |t|
-    t.integer "user_id"
+  create_table "song_playlists", force: :cascade do |t|
+    t.integer "playlist_id"
     t.integer "song_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "song_url"
+    t.string "code"
+    t.string "title"
+    t.string "genre"
+    t.string "img"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
