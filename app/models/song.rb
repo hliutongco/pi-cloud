001ca7b458ed7563ad53bbce88a7bf3354ee4e4a
Helen Liutongco <liutongcoh@gmail.com>
2018-04-17 10:49:51 -0400

    Add validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,14 @@
 class Song < ApplicationRecord
   include SongUploader[:url]
 
+  validates :title, presence: true
+  validates :url, presence: true
+
   belongs_to :user
   has_many :song_playlists
   has_many :playlists, through: :song_playlists
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment :avatar,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
